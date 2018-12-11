2e0ebd7e88810fc9a0f7c35f1ec24bf50793f903
Evgeniy Poznyak <evgeniyp@fandiexpress.com>
2018-12-11 16:47:42 -0600

    Added dog & cat images

--- src/app/cats/cats.component.ts
+++ src/app/cats/cats.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cats.component.sass']
 })
 export class CatsComponent implements OnInit {
+  catImages: string[] = [
+    'https://media.mnn.com/assets/images/2018/07/cat_eating_fancy_ice_cream.jpg.838x0_q80.jpg',
+    'https://i.ytimg.com/vi/lrvqjdMcjjQ/hqdefault.jpg',
+    'http://honesttopaws.com/wp-content/uploads/sites/5/2017/05/banana-cat-1.png',
+    'https://static.boredpanda.com/blog/wp-content/uploads/2018/04/han' +
+    'dicapped-cat-rexie-the-handicat-dasha-minaeva-58-5acb4f1931e1b__700.jpg',
+  ];
 
   constructor() { }
 
--- src/app/dogs/dogs.component.ts
+++ src/app/dogs/dogs.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dogs.component.sass']
 })
 export class DogsComponent implements OnInit {
-
+  dogImages: string[] = [
+    'https://www.dogster.com/wp-content/uploads/2017/11/A-dog-looking-up-confused-nervous-anxious.jpg',
+    'https://amp.businessinsider.com/images/5b22c10042e1cc25fc535e38-750-563.jpg',
+    'http://images.tritondigitalcms.com/3922/2018/09/24122900/dog.jpg',
+    'https://dynaimage.cdn.cnn.com/cnn/w_768,h_1024,c_scale/https%3A%2F%2Fdynaimage.cdn.cnn.com%2Fcnn%2Fx_1229%2Cy_0%2Cw_2712%' +
+    '2Ch_3616%2Cc_crop%2Fhttps%253A%252F%252Fstamp.static.cnn.io%252F5b7ac48b4db3d70020c01c13%252Fshutterstock_1081879181.jpg',
+  ];
   constructor() { }
 
   ngOnInit() {
