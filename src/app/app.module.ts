755df2afd81dab16c5c3265015eca21f737a3d37
Evgeniy Poznyak <evgeniyp@fandiexpress.com>
2018-12-11 16:33:18 -0600

    Created cats and dogs components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatsComponent } from './cats/cats.component';
+import { DogsComponent } from './dogs/dogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatsComponent,
+    DogsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cats/cats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cats',
+  templateUrl: './cats.component.html',
+  styleUrls: ['./cats.component.sass']
+})
+export class CatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
