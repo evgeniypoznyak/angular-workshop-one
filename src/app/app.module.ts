0f476ce348028c8c50026bd78adc2b7cda5150db
Evgeniy Poznyak <evgeniyp@fandiexpress.com>
2018-12-11 16:37:11 -0600

    Create a singe cat and dog subcomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatsComponent } from './cats/cats.component';
 import { DogsComponent } from './dogs/dogs.component';
+import { CatComponent } from './cats/cat/cat.component';
+import { DogComponent } from './dogs/dog/dog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CatsComponent,
-    DogsComponent
+    DogsComponent,
+    CatComponent,
+    DogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dogs/dog/dog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dog',
+  templateUrl: './dog.component.html',
+  styleUrls: ['./dog.component.sass']
+})
+export class DogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
